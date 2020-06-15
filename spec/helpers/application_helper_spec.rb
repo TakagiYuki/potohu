5e9fdf1a2b80e3fcc5b6bf538793e524ad62fba8
Takagi Yuki <yuyuyu.eievui@icloud.com>
2020-06-15 03:58:17 +0000

    develop

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  #タイトルのテスト
   describe "#full_title" do
     context "page_title is empty" do
       it "removes symbol" do
@@ -9,10 +10,12 @@ RSpec.describe ApplicationHelper, type: :helper do
       end
     end
 
+    #サブタイトルのテスト.
     context "page_title is not empty" do
       it "returns title and application name where contains symbol" do
         expect(helper.full_title('サブタイトル')).to eq('サブタイトル | ポトフ')
       end
     end
+
   end
 end
