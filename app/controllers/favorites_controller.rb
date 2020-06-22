511d09a6535bac253cbbd93642fa7703ca4a3f5b
Takagi Yuki <yuyuyu.eievui@icloud.com>
2020-06-22 23:23:58 +0000

    update-favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,23 @@
 class FavoritesController < ApplicationController
+  before_action :set_event
+
   def create
     event = Event.find(params[:event_id])
     favorite = Favorite.new()
     favorite.user_id = current_user.id
     favorite.event_id = event.id
     favorite.save
-    redirect_to events_path
+    # redirect_to events_path
   end
 
   def destroy
     event = Event.find(params[:event_id])
     favorite = current_user.favorites.find_by(event_id: event.id)
     favorite.destroy
-    redirect_to events_path
+    # redirect_to events_path
   end
+
+  def set_event
+   @event = Event.find(params[:event_id])
+ end
 end
