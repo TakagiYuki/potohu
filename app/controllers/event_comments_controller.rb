38ac23d97e187479d7760dcbe94f00440b09f6cc
Takagi Yuki <yuyuyu.eievui@icloud.com>
2020-06-24 11:50:33 +0000

    update-comment

--- app/controllers/event_comments_controller.rb
+++ app/controllers/event_comments_controller.rb
@@ -1,13 +1,25 @@
 class EventCommentsController < ApplicationController
   def create
   	# (comment:□, user_id□, event_id□)
-  	event = Event.find(params[:event_id])
-    comment =EventComment.new(event_comment_params)
-    comment.user_id = current_user.id
-	  comment.event_id = event.id
-	  comment.save
-	  redirect_to root_url
+  	@event = Event.find(params[:event_id])
+    @event_comment =EventComment.new(event_comment_params)
+    @event_comment.user_id = current_user.id
+	  @event_comment.event_id = @event.id
+	  @event_comment.save
   end
+
+
+# def create
+#     @event = Event.find(params[:event_id])
+#     @event_new = Event.new
+#     @evevnt_comment = @event.event_comments.new(event_comment_params)
+#     @event_comment.user_id = current_user.id
+#     if @event_comment.save
+#       flash[:success] = "Comment was successfully created."
+#     else
+#       @event_comments = EventComment.where(id: @event)
+#     end
+#   end
 
   private
 	def event_comment_params
@@ -15,3 +27,4 @@ class EventCommentsController < ApplicationController
 	end
 
 end
+
