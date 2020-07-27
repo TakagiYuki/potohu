a8004dfe978b8c0fc198236e12bbe7ce55012483
Takagi Yuki <yuyuyu.eievui@icloud.com>
2020-07-27 14:26:58 +0000

    add-test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   it "イベントがある場合、有効である" do
+
+  	area = Area.new(
+      name: "北海道"
+    )
+    area.save()
+
     event = Event.new(
       name:"東京タワー",
       article:"333メートルの電波塔",
@@ -12,7 +18,7 @@ RSpec.describe Event, type: :model do
 	  is_valid:"true",
 	  open_time:"2020-2-22",
 	  close_time:"2020-2-22",
-	  area_id:"1",
+	  area_id: area.id,
 	  latitude: "123",
       longitude: "456"
       )
