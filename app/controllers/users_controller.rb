2e4def96d308e938ca98bc3402e69c08ca420cda
TakagiYuki <yuyuyu.eievui@gmail.com>
2020-08-23 10:57:56 +0000

    fix sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      redirect_to @user
+      redirect_to root_url
     else
       render 'new'
     end
